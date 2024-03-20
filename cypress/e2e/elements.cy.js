fb0cec4cc2597785e5eb0435334da8060768c616
Diego Silva Costa <diegosemeao2@gmail.com>
2024-03-20 10:59:58 -0300

    Atualização

--- cypress/e2e/elements.cy.js
+++ cypress/e2e/elements.cy.js
@@ -1,12 +1,30 @@
 /// <reference types = "Cypress" />
 
 describe ('Work with basic elements', () => {
-    it('Text', () => {
+    beforeEach(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.reload()
 
+    });
+    
+
+
+    it('Text', () => {
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
         cy.get(".facilAchar").should('have.text', 'Cuidado onde clica, muitas armadilhas...')
-    });
+    })
 
-})
+    it('Links', () => {
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should("have.text", "Voltou!")
+
+
+    cy.reload()
+    cy.get("#resultado").should("have.not.text", "Voltou!")  //Negativa 
+    cy.contains('Voltar').click()
+    cy.get('#resultado').should('have.text', 'Voltou!')
+
+    })
+
+}) 
