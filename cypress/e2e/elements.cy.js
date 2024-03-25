2c3c8664a792968d007ab8ebeb92dab34fc74af2
Diego Silva Costa <diegosemeao2@gmail.com>
2024-03-25 10:29:38 -0300

    new

--- cypress/e2e/elements.cy.js
+++ cypress/e2e/elements.cy.js
@@ -3,9 +3,9 @@
 describe ('Work with basic elements', () => {
     beforeEach(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
-        cy.reload()
-
-    });
+    })
+    
+    
     
 
 
@@ -19,12 +19,42 @@ describe ('Work with basic elements', () => {
         cy.get('[href="#"]').click()
         cy.get('#resultado').should("have.text", "Voltou!")
 
-
-    cy.reload()
-    cy.get("#resultado").should("have.not.text", "Voltou!")  //Negativa 
+ 
     cy.contains('Voltar').click()
     cy.get('#resultado').should('have.text', 'Voltou!')
 
     })
+    it('TextFields', () => {
+
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value','Cypress Test')
+
+        cy.get('#elementosForm\\:sugestoes')
+        .type("textarea")
+        .should('have.value', 'textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+        .type('???')
+        .should('have.value', '???')
+
+        cy.get('[data-cy="dataSobrenome"]')
+        .type('Teste12345{backspace}{backspace}')
+        .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+        .clear()
+        .type("Erro{selectall}acerto" , { delay:100 })
+        .should('have.value', 'acerto')
+    });
+
+    it('Radiobutton', () => {
+        cy.get('#formSexoFem')
+        .click()
+        .should('be.checked')
+
+        cy.get('#formSexoMasc').should('not.be.checked')
+
+
+    });
 
 }) 
