d2f586849d393bce37c55d5a1c7d9e91f28cfc00
Diego Silva Costa <diegosemeao2@gmail.com>
2024-04-08 12:51:32 -0300

    new

--- cypress/e2e/sync.cy.js
+++ cypress/e2e/sync.cy.js
@@ -15,4 +15,27 @@ describe ('Work with basic elements', () => {
         cy.get('#novoCampo').type('funciona')
         
     })
+    it('Deve fazer retrys', () => {
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo')
+        //.should('not.exist')
+        .should('exist')
+        .type('funciona')
+
+     
+    })
+    it('Uso do find', () => {
+        cy.get("#buttonlist").click()
+        cy.get("#lista li")
+        .find('span')
+        .should('contain', 'Item 1')
+        cy.get("#lista li span")
+        .should('contain', 'Item 2')
+        
+    })
+    it('Uso do timeOut', () => {
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo', {timeout: 30000}).should('exist')
+    });
+
 })
