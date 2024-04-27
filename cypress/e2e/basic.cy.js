7cb6213fb9ea391435b518f86b6c0cefac51fc96
Diego Silva Costa <129333423+DiegoCostax@users.noreply.github.com>
2024-04-27 10:06:27 -0300

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -2,7 +2,7 @@
 /* global cy */
 
 describe ('Cypress basics', () => {
-    it.only('Should visit a page and assert title', () =>{
+    it('Should visit a page and assert title', () =>{
        cy.visit('https://wcaquino.me/cypress/componentes.html')
 
 
